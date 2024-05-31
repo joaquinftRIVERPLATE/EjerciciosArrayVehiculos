17088f3812c1dd1b3672a48ae7c3a242751f3ff5
joaquinftRIVERPLATE <joaquisft@gmail.com>
2024-05-31 09:04:37 -0300

    reset vehicles; press R

---
+++ Assets/Scripts/AlquilerVehiculos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlquilerVehiculos : MonoBehaviour
+{
+    public GameObject[] vehiculos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetearVehiculos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            DesactivcarTodosLosElementos();
+            vehiculos[Random.Range(0, vehiculos.Length)].SetActive(true);
+        }   
+    }
+
+    void ResetearVehiculos()
+    {
+        DesactivcarTodosLosElementos();
+        vehiculos[0].SetActive(true);
+    }
+    void DesactivcarTodosLosElementos()
+    {
+        for (int i = 0; i < vehiculos.Length; i++)
+        {
+            vehiculos[i].SetActive(false);
+        }
+        
+    }
+}
